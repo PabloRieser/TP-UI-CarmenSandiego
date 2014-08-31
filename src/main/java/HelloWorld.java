6213f73b9ed8b8906ab1c77b04fb5e2113e4c1eb
Pablo Rieser <pablo.rieser@live.com.ar>
2014-08-30 23:39:22 -0300

    Initial commit. Creando la estructura de directorios.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Escritorio on 30/08/2014.
+ */
+public class HelloWorld {
+
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Escritorio on 30/08/2014.
+ */
+public class HelloWorldTest {
+}
